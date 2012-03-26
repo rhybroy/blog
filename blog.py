8b782c77be2ce59021598cc02ac6526203049877
lucas <lucas@.(none)>
2012-03-26 21:48:15 +0800

    first commit

---
+++ blog.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+
+import os.path
+import tornado.auth
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+from tornado.options import define, options, parse_command_line
+
+import urllib
+import markdown2
+
+import time
+
+import admin
+
+import settings as setting_parm
+
+class Application(tornado.web.Application):
+
+	def __init__(self):
+		handlers = [
+			(r"/", HomeHandler),
+			(r"/page/(\d+)/?", HomeHandler),
+			(r"/category/([^/]+)/?", CategoryHandler),
+			(r"/category/([^/]+)/(\d+)?/?", CategoryHandler),
+			(r"/search/([^/]+)/?", SearchHandler),
+			(r"/search/([^/]+)/(\d+)?/?", SearchHandler),
+			(r"/entry/([^/]+)", EntryHandler),
+			(r"/feed", FeedHandler),
+			(r"/compose", admin.ComposeHandler),
+			(r"/admin/list", admin.ListHandler),
+			(r"/auth/login", admin.AuthLoginHandler),
+			(r"/auth/logout", admin.AuthLogoutHandler),
+			(r"/admin/metalist", admin.MetaListHandler),
+			(r"/admin/meta", admin.MetaHandler),
+		]
+		settings = dict(
+			blog_title=u"Tornado Blog",
+			template_path=os.path.join(os.path.dirname(__file__), "templates"),
+			static_path=os.path.join(os.path.dirname(__file__), "static"),
+			ui_modules={"Entry": EntryModule},
+			xsrf_cookies=True,
+			cookie_secret="11oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+			login_url="/auth/login",
+			autoescape=None,
+		)
+		tornado.web.Application.__init__(self, handlers, **settings)
+		#self.db = lemondb.connect("mysql", host="localhost", user="root", passwd="", db="blog", charset="utf8")
+		
+		import sys
+		sys.path.append("dbutil")
+		import lemondb
+		self.db = lemondb.connect(options.database, **setting_parm.database_types[options.database])
+
+class BaseHandler(tornado.web.RequestHandler):
+	_start_time = time.time()
+	_finish_time = None
+
+	def request_time(self):
+		"""Returns the amount of time it took for this request to execute."""
+		if self._finish_time is None:
+			return time.time() - self._start_time
+		else:
+			return self._finish_time - self._start_time
+	
+
+	@property
+	def db(self):
+		return self.application.db
+
+	def get_current_user(self):
+		user_id = self.get_secure_cookie("user")
+		if not user_id: return None
+		return self.db.get("SELECT * FROM typecho_users WHERE name = %s", str(user_id))
+		
+
+
+class HomeHandler(BaseHandler):
+	def get(self, page=1):
+		keyword = self.get_argument("keyword", None);
+		if keyword:
+			self.redirect("/search/%s"%keyword)
+			return
+		pagesize = 2
+		page = int(page)
+		if page < 1:
+			page = 1
+		start = (page-1)*pagesize
+		entries = self.db.query("SELECT a.*, c.name as category, c.slug as cslug FROM typecho_contents a, typecho_relationships b, typecho_metas c where a.status = 'publish' and a.cid = b.cid and b.mid = c.mid ORDER BY a.created "
+								"DESC LIMIT %s, %s", start, pagesize)
+		
+		categorys = self.db.query("select * from typecho_metas where type='category' order by `order`")
+		recentlys = self.db.query("select title, slug, created from typecho_contents order by created desc limit 10")
+		
+		self.render("index.html", entries=entries, nextpage=page+1, prepage=page-1, pagetype="page", categorys=categorys, recentlys=recentlys)
+
+
+class EntryHandler(BaseHandler):
+	def get(self, slug):
+		if isinstance(slug, unicode):
+			slug = slug.encode("utf8")
+		slug = urllib.quote(slug).lower().strip()
+
+		entry = self.db.get("SELECT * FROM typecho_contents WHERE slug = %s and status = 'publish'", slug)
+		if not entry: 
+			raise tornado.web.HTTPError(404)
+		
+		comments = self.db.query("select * from typecho_comments where cid = %s order by created", entry.cid)
+		
+		categorys = self.db.query("select * from typecho_metas where type='category' order by `order`")
+		recentlys = self.db.query("select title, slug, created from typecho_contents order by created desc limit 10")
+		
+		self.render("entry.html", entry=entry, comments=comments, categorys=categorys, recentlys=recentlys)
+
+
+class CategoryHandler(BaseHandler):
+	def get(self, slug, page=1):
+		if isinstance(slug, unicode):
+			slug = slug.encode("utf8")
+		slug = urllib.quote(slug).lower().strip()
+		mid = self.db.get("select mid from typecho_metas where slug = %s", slug).mid
+		pagesize = 2
+		page = int(page)
+		if page < 1:
+			page = 1
+		start = (page-1)*pagesize
+		entries = self.db.query("SELECT a.*, c.name as category, c.slug as cslug FROM typecho_contents a, typecho_relationships b, typecho_metas c where a.cid = b.cid and b.mid = c.mid and b.mid = %s and a.status = 'publish' ORDER BY created DESC LIMIT %s, %s", mid, start, pagesize)
+		
+		categorys = self.db.query("select * from typecho_metas where type='category' order by `order`")
+		recentlys = self.db.query("select title, slug, created from typecho_contents order by created desc limit 10")
+		
+		self.render("index.html", entries=entries, nextpage=page+1, prepage=page-1, pagetype="category/%s"%slug, categorys=categorys, recentlys=recentlys)
+
+class SearchHandler(BaseHandler):
+	def get(self, keyword, page=1):
+		pagesize = 2
+		page = int(page)
+		if page < 1:
+			page = 1
+		start = (page-1)*pagesize
+		entries = self.db.query("SELECT a.*, c.name as category, c.slug as cslug FROM typecho_contents a, typecho_relationships b, typecho_metas c where a.cid = b.cid and b.mid = c.mid and a.title like %s ORDER BY a.created DESC LIMIT %s, %s", "%"+keyword+"%", start, pagesize)
+		
+		categorys = self.db.query("select * from typecho_metas where type='category' order by `order`")
+		recentlys = self.db.query("select title, slug, created from typecho_contents order by created desc limit 10")
+		
+		self.render("index.html", entries=entries, nextpage=page+1, prepage=page-1, pagetype="search/%s"%keyword, categorys=categorys, recentlys=recentlys)
+
+class FeedHandler(BaseHandler):
+	def get(self):
+		entries = self.db.query("SELECT * FROM entries ORDER BY published "
+								"DESC LIMIT 10")
+		self.set_header("Content-Type", "application/atom+xml")
+		self.render("feed.xml", entries=entries)
+
+
+
+class EntryModule(tornado.web.UIModule):
+	def render(self, entry):
+		return self.render_string("modules/entry.html", entry=entry)
+
+
+def main():
+	tornado.options.parse_command_line()
+	http_server = tornado.httpserver.HTTPServer(Application())
+	http_server.listen(options.port)
+	tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+	main()
---
+++ dbutil/lemondb.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+
+import itertools
+import logging
+import time
+import sys
+import os
+
+def connect( jclassname, **args ):
+	#sys.path.append(os.getcwd()+'')
+	if(jclassname == "mysql"):
+		mymod = __import__("mysqldb")
+		return mymod._getobj(**args)
+		#return mysqldb(args)
+	elif(jclassname == "sqlite"):
+		mymod = __import__("sqlitedb")
+		return mymod._getobj(**args)
+
+class LemonDB(object):
+	def __init__(self, **kvargs):
+		#print 'base class __init__'
+		max_idle_time=7*3600
+		self.max_idle_time = max_idle_time
+		self._db_args = kvargs
+		self._last_use_time = time.time()
+		self._connect()
+
+	def __del__(self):
+		self.close()
+		#print 'base class close'
+
+	def close(self):
+		"""Closes this database connection."""
+		if getattr(self, "_conn", None) is not None:
+			self._conn.close()
+			self._conn = None
+		#else:
+			#print 'no have _conn attr'
+
+	def reconnect(self):
+		"""Closes the existing database connection and re-opens it."""
+		self.close()
+		self._connect()
+		self._conn.autocommit(True)
+
+	def query(self, query, *parameters):
+		"""Returns a row list for the given query and parameters."""
+		cursor = self._excursor()
+		try:
+			self._execute(cursor, query, parameters)
+			column_names = [d[0] for d in cursor.description]
+			return [Row(itertools.izip(column_names, row)) for row in cursor]
+		finally:
+			self.close()
+
+	def get(self, query, *parameters):
+		"""Returns the first row returned for the given query."""
+		rows = self.query(query, *parameters)
+		if not rows:
+			return None
+		else:
+			return rows[0]
+		
+	def getint(self, query, *parameters):
+		"""Returns the first row returned for the given query."""
+		cursor = self._excursor()
+		try:
+			self._execute(cursor, query, parameters)
+			for row in cursor:
+				if row[0]:
+					return int(row[0])
+				else:
+					return 0
+		finally:
+			self.close()
+		
+	def checkExist(self, query, *parameters):
+		"""Returns the first row returned for the given query."""
+		rows = self.query(query, *parameters)
+		if not rows:
+			return False
+		else:
+			return True
+			
+	def getone(self, query, *parameters):
+		"""Returns the first row returned for the given query."""
+		rows = self.query(query, *parameters)
+		if not rows:
+			return None
+		else:
+			if len(rows) > 1:
+				raise Exception("Multiple rows returned for Database.get() query")
+			else:
+				return rows[0]
+
+	def execute(self, query, *parameters):
+		"""Executes the given query, returning the lastrowid from the query."""
+		cursor = self._excursor()
+		try:
+			self._execute(cursor, query, parameters)
+			return cursor.lastrowid
+		finally:
+			self.close()
+
+	def executemany(self, query, parameters):
+		"""Executes the given query against all tqueryhe given param sequences.
+
+		We return the lastrowid from the query.
+		"""
+		cursor = self._excursor()
+		try:
+			cursor.executemany(query, parameters)
+			return cursor.lastrowid
+		finally:
+			self.close()
+
+	def _ensure_connected(self):
+		# Mysql by default closes client connections that are idle for
+		# 8 hours, but the client library does not report this fact until
+		# you try to perform a query and it fails.  Protect against this
+		# case by preemptively closing and reopening the connection
+		# if it has been idle for too long (7 hours by default).
+		if (self._conn is None or
+			(time.time() - self._last_use_time > self.max_idle_time)):
+			self.reconnect()
+		self._last_use_time = time.time()
+
+	def _excursor(self):
+		self._ensure_connected()
+		return self._cursor
+
+	def _execute(self, cursor, query, parameters):
+		try:
+			return cursor.execute(query, parameters)
+		except RuntimeError:
+			logging.error("Error connecting to MySQL on %s", self.host)
+			self.close()
+			raise
+
+
+class Row(dict):
+	"""A dict that allows for object-like property access syntax."""
+	def __getattr__(self, name):
+		try:
+			return self[name]
+		except KeyError:
+			raise AttributeError(name)
+
+
+	
+
+if __name__ == '__main__':
+	#db = connect("sqlite", db="e:/sqlite/blog.db")
+	db = connect("mysql", host="localhost", user="root", passwd="", db="blog")
+	
+	result = db.getint("select count(*) from typecho_contents")
+	print result
+	#result = db.execute("insert into entries(id, title) values(%s, %s)", 5, 'test');
+	#print result
